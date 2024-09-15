4885bb87aeef534e42fd7f7a85539296fe4a4291
Jaloliddin08 <j.qulmatov@newuu.uz>
2024-09-15 23:44:48 +0500

    add problem3

--- problem3.cpp
+++ problem3.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 int main() {
     double celsius;
-    cout << "Enter the temperature in Celsius: ";
     cin >> celsius;
     double fahrenheit = (celsius * 9.0 / 5.0) + 32;
-    cout << "The temperature in Fahrenheit is equal to " << fahrenheit << endl;
+     cout << "The temperature in Fahrenheit is equal to " << fahrenheit << endl;
 
     return 0;
 }
